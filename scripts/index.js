6b94db80d7f710cf29dbd0919acb8c157fd93c5b
DmitrySkorr <142891508+DmitrySkorr@users.noreply.github.com>
2023-12-08 13:33:07 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,20 +3,20 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function cardAdd(name, link, cardDelete) {
+function cardAdd(name, link, Deletecard) {
     const cards = cardTemplate.querySelector('.card').cloneNode(true);
     const delButton = cards.querySelector('.card__delete-button');
     cards.querySelector('.card__image').alt = name;
     cards.querySelector('.card__image').src = link;
     cards.querySelector('.card__title').textContent = name;
-    delButton.addEventListener('click', cardDelete);
+    delButton.addEventListener('click', Deletecard);
     return cards;
 }
 // @todo: Функция удаления карточки
-function cardDelete(evt) {
+function Deletecard(evt) {
     evt.target.closest('.places__item').remove();
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (element) {
-    placesList.append(cardAdd(element.name, element.link, cardDelete));
+    placesList.append(cardAdd(element.name, element.link, Deletecard));
 })
